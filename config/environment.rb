409324697c916cf3d46153c1d3c6617e1d1725f6
msuzoagu <msuzoagu@gmail.com>
2015-04-16 11:56:56 -0400

    add access to DATABASE_URL

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,6 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
 require_all 'app'
