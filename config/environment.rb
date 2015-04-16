76991c5d1dc7fe40b786f903706beb934f5a7df6
msuzoagu <msuzoagu@gmail.com>
2015-04-16 12:22:56 -0400

    add ruby version

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,16 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+configure :production, :development do
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/sinatra_pigeon_development')
+ActiveRecord::Base.establish_connection(
+  adapter: db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  host: db.host,
+  username: db.user,
+  password: db.password,
+  database: db.path[1..-1],
+  encoding: 'utf8'
+)
+end
 
 require_all 'app'
